2a399d0e1e63b636dba4b851deb2d1000d174564
nishikai <nishizatokaisyuu0305@gmail.com>
2026-06-18 23:12:25 +0900

    SQL実行

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 
-require_once "../config/database.php";
+// require_once "../config/database.php";
+require_once __DIR__ . "/../config/database.php";
 
+
+$sql = "select * from todos";
+$stmt = $pdo->query($sql);
+$todos = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+
+// データ確認
 echo "DB接続成功";
+echo "<pre>";
+print_r($todos);
+echo "</pre>";
